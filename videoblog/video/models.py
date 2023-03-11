2b2cca94ca19c611d62fe4c13bac211a9593cadd
pwnd27 <thebuff27@mail.ru>
2023-03-11 17:01:05 +0700

    add model video

--- videoblog/video/models.py
+++ videoblog/video/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    description = models.TextField()
+    url = models.FileField(max_length=50, null=True, blank=True)
+    cover = models.ImageField(max_length=50, null=True, blank=True)
