45d8737d819a917f221fc313f2a41c45fb73a922
pwnd27 <thebuff27@mail.ru>
2023-03-19 15:54:01 +0700

    add validation video file

--- videoblog/video/forms.py
+++ videoblog/video/forms.py
@@ -1,8 +1,9 @@
-from django.forms import ModelForm
+from django import forms
 from video.models import Video
+import os
 
 
-class VideoForm(ModelForm):
+class VideoForm(forms.ModelForm):
 
     class Meta:
         model = Video
@@ -12,3 +13,11 @@ class VideoForm(ModelForm):
             'file',
             'cover',
         ]
+
+    def clean_file(self):
+        file = self.cleaned_data.get('file', None)
+        if file:
+            ext = os.path.splitext(file.name)[1].lower()
+            if ext != '.mp4':
+                raise forms.ValidationError('Файл должен быть в формате MP4')
+            return file
