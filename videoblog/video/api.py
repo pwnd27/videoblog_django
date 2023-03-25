b9af07897986d778dc72d14a24b75fad5b23108f
pwnd27 <thebuff27@mail.ru>
2023-03-25 23:41:07 +0700

    add video crud

--- videoblog/video/api.py
+++ videoblog/video/api.py
@@ -1,8 +1,39 @@
 from rest_framework import generics
+from rest_framework import status
 from video.models import Video
 from video.serializers import VideoSerializer
+from rest_framework.response import Response
+import os
 
 
-class VideoAPIView(generics.ListAPIView):
+class VideoListAPIView(generics.ListCreateAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+
+    def perform_create(self, instance):
+        instance.save(user=self.request.user)
+
+
+class VideoDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+    def perform_destroy(self, instance):
+        video_path = instance.file.path
+        cover_path = instance.cover.path
+        if os.path.isfile(cover_path):
+            os.remove(cover_path)
+        if os.path.isfile(video_path):
+            os.remove(video_path)
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_update(self, instance):
+        new_file = self.request.data.get('file', None)
+        new_cover = self.request.data.get('cover', None)
+        if new_file:
+            instance.file = new_file
+        if new_cover:
+            instance.cover = new_cover
+        instance.save()
+        return Response(instance.data)
--- videoblog/video/urls.py
+++ videoblog/video/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('<int:pk>/', views.VideoDetailView.as_view(), name='video-detail'),
     path('update/<int:pk>/', views.VideoUpdateView.as_view(), name='video-update'),
     path('delete/<int:pk>/', views.VideoDeleteView.as_view(), name='video-delete'),
-    path('api/', api.VideoAPIView.as_view(), name='video-list-api')
+    path('api/', api.VideoListAPIView.as_view(), name='video-list-api'),
+    path('api/<int:pk>/', api.VideoDetailAPIView.as_view(), name='video-detail-api'),
 ]
